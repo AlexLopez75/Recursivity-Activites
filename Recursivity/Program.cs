b062227bc284dabb23e2e6e1d82a0dc54fe10435
AlexLopez75 <alex.lopez.7e9@itb.cat>
2026-01-13 13:13:30 +0100

    add: added program and method library

---
+++ Recursivity/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.NetworkInformation;
+using RecursivityLibrary;
+namespace Recursivity
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            RecursLibrary.Countdown(4);
+            Console.WriteLine(RecursLibrary.CountDigits(120));
+            Console.WriteLine(RecursLibrary.AddDigits(123));
+            Console.WriteLine(RecursLibrary.Swap(1234));
+        }
+    }
+}
