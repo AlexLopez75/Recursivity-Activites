05500c16eef3769fd3945aa7113ae3811f3b54e4
AlexLopez75 <alex.lopez.7e9@itb.cat>
2026-01-15 10:10:25 +0100

    add: added code for exerciseTwo

--- Recursivity/Program.cs
+++ Recursivity/Program.cs
@@ -8,8 +8,6 @@ namespace Recursivity
         {
             RecursLibrary.Countdown(4);
             Console.WriteLine(RecursLibrary.CountDigits(120));
-            Console.WriteLine(RecursLibrary.AddDigits(123));
-            Console.WriteLine(RecursLibrary.Swap(1234));
         }
     }
 }
