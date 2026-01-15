e3fc1ec24518e0bdb706032f487322374dfbaa8e
AlexLopez75 <alex.lopez.7e9@itb.cat>
2026-01-15 11:53:24 +0100

    add: added code for exerciseFive

--- Recursivity/Program.cs
+++ Recursivity/Program.cs
@@ -1,15 +1,21 @@
-﻿using System.Net.NetworkInformation;
-using RecursivityLibrary;
+﻿using RecursivityLibrary;
+using System.Collections;
+using System.Net.NetworkInformation;
 namespace Recursivity
 {
     public class Program
     {
         public static void Main()
         {
+            int[] arrayInt = { 3, 2, 4, 1, 5 };
+
             RecursLibrary.Countdown(4);
             Console.WriteLine(RecursLibrary.CountDigits(120));
             Console.WriteLine(RecursLibrary.AddDigits(123));
             Console.WriteLine(RecursLibrary.Swap(1234));
+            RecursLibrary.PrintForwardArray(arrayInt, 0);
+            Console.WriteLine();
+            RecursLibrary.PrintBackwardArray(arrayInt, 4);
         }
     }
 }
